47b322a55f4cd7ac4accfe2d93643dd0af7d9f8a
Adrian Moring <adriantmoringbass@gmail.com>
2026-04-14 11:52:25 -0400

    changed config route

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
-from config import app, db, api
+from .config import app, db, api
 from models import User, Recipe, UserSchema, RecipeSchema
 
 class Signup(Resource):
