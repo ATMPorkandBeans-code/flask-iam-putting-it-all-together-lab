2309f7fdce71da0c571160e260cb95d94fe0ecb8
Adrian Moring <adriantmoringbass@gmail.com>
2026-04-14 11:59:13 -0400

    fixed imports

--- server/app.py
+++ server/app.py
@@ -4,8 +4,8 @@ from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
-from .config import app, db, api
-from models import User, Recipe, UserSchema, RecipeSchema
+from server.config import app, db, api
+from server.models import User, Recipe, UserSchema, RecipeSchema
 
 class Signup(Resource):
    def post(self):
