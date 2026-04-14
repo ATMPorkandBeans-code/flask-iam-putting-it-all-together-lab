10f393c0ef1ef38604b586c41601dd7100ba63b5
Adrian Moring <adriantmoringbass@gmail.com>
2026-04-14 12:03:59 -0400

    fixed port issue

--- server/app.py
+++ server/app.py
@@ -100,5 +100,5 @@ api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
